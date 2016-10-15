9d7e47f5df145b8163735917ef4a53319847e590
Beljaev_Pavel <beljaev.pavel.kr@gmail.com>
2016-10-15 21:30:25 +0200

    init Project

---
+++ src/main/java/com/pesahlavan/training/ws/RestAdrBookSpringBootJfxApplication.java
@@ -0,0 +1,12 @@
+package com.pesahlavan.training.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAdrBookSpringBootJfxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestAdrBookSpringBootJfxApplication.class, args);
+	}
+}
