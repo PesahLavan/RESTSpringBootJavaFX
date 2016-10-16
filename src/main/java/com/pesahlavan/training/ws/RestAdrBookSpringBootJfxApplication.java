bf7782d54573884b1c785834be85b268b8bc4465
Beljaev_Pavel <beljaev.pavel.kr@gmail.com>
2016-10-16 12:47:17 +0200

    init db

--- src/main/java/com/pesahlavan/training/ws/RestAdrBookSpringBootJfxApplication.java
+++ src/main/java/com/pesahlavan/training/ws/RestAdrBookSpringBootJfxApplication.java
@@ -3,7 +3,7 @@ package com.pesahlavan.training.ws;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.pesahlavan.training"})
 public class RestAdrBookSpringBootJfxApplication {
 
 	public static void main(String[] args) {
