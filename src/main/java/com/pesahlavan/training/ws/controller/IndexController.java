a10661afbdb5843e8160d1407cd077cdba338fa1
Beljaev_Pavel <beljaev.pavel.kr@gmail.com>
2016-10-16 17:17:59 +0200

    init read DB

--- src/main/java/com/pesahlavan/training/ws/controller/IndexController.java
+++ src/main/java/com/pesahlavan/training/ws/controller/IndexController.java
@@ -1,15 +1,25 @@
 package com.pesahlavan.training.ws.controller;
 
 
+import com.pesahlavan.training.javafx.entity.Person;
+import com.pesahlavan.training.javafx.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping (value = "/addressbook")
 public class IndexController {
 
-    @RequestMapping (value = "/")
-    public String index(){
-        return "test page";
+    @Autowired
+    private PersonRepository personRepository;
+
+
+    @RequestMapping (value = "/all")
+    public List<Person> findAll() {
+        return personRepository.findAll();
     }
-    
+
 }
