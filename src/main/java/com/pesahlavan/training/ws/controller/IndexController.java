282d6bd21bd4c5c65cd1fd36a1f5c56bd73bb46c
Beljaev_Pavel <beljaev.pavel.kr@gmail.com>
2016-10-16 18:04:37 +0200

    init read DB

--- src/main/java/com/pesahlavan/training/ws/controller/IndexController.java
+++ src/main/java/com/pesahlavan/training/ws/controller/IndexController.java
@@ -3,11 +3,14 @@ package com.pesahlavan.training.ws.controller;
 
 import com.pesahlavan.training.javafx.entity.Person;
 import com.pesahlavan.training.javafx.repository.PersonRepository;
+import com.pesahlavan.training.javafx.service.ListWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping (value = "/addressbook")
@@ -18,8 +21,17 @@ public class IndexController {
 
 
     @RequestMapping (value = "/all")
-    public List<Person> findAll() {
-        return personRepository.findAll();
+    public ListWrapper findAll(@RequestParam(value="from", defaultValue="0") int from, @RequestParam(value="count", defaultValue="10") int count){
+        Page<Person> page = personRepository.findAll(new PageRequest(from, count, Sort.Direction.ASC, "fio"));
+        ListWrapper wrappper = new ListWrapper(page.getContent(), page.getTotalElements());
+        return wrappper;
+    }
+
+    @RequestMapping (value = "/search")
+    public ListWrapper findAll(@RequestParam(value="from", defaultValue="0") int from, @RequestParam(value="count", defaultValue="10") int count, @RequestParam(value="text", defaultValue="") String text) {
+        Page<Person> page = personRepository.findByFioContainingIgnoreCase(text, new PageRequest(from, count, Sort.Direction.ASC, "fio"));
+        ListWrapper wrappper = new ListWrapper(page.getContent(), page.getTotalElements());
+        return wrappper;
     }
 
 }
